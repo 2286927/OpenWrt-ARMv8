2c82849164937f17d63035864f0a07aa77e9bb1b
ChenChen <cc2286927@sina.cn>
2022-05-29 16:26:06 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -14,7 +14,7 @@
 # sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/files/bin/config_generate
 
 # Hostname
-sed -i 's/OpenWrt/qnmlgb/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenWrt-D2550/g' package/base-files/files/bin/config_generate
 
 # Timezone
 #sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
@@ -24,8 +24,8 @@ sed -i 's/OpenWrt/qnmlgb/g' package/base-files/files/bin/config_generate
 # sed -i 's/services/system/g' package/lean/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 
 # Change default theme
-sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/luci/Makefile
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#sed -i 's#luci-theme-bootstrap#luci-theme-opentomcat#g' feeds/luci/collections/luci/Makefile
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # Add additional packages
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
