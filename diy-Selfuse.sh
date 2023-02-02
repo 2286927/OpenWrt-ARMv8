676d3b5fc78ada1ebc65ab333c2d639ae8f4bdf6
ChenChen <cc2286927@sina.cn>
2023-02-02 17:22:47 +0800

    Update diy-Selfuse.sh

--- diy-Selfuse.sh
+++ diy-Selfuse.sh
@@ -51,8 +51,8 @@ sed -i 's/services/system/g' feeds/luci/applications/luci-app-cpufreq/luasrc/con
 # rm -rf ./package/small-package/dockerd
 # rm -rf ./package/small-package/luci-app-dockerman
 rm -rf ./feeds/packages/utils/dockerd
-rm -rf ./package/feeds/luci/luci-app-dockerman
-rm -rf ./package/feeds/luci/luci-app-docker
+rm -rf ./feeds/luci/luci-app-dockerman
+rm -rf ./feeds/luci/luci-app-docker
 rm -rf ./package/small-package/luci-app-tencentcloud-cos
 rm -rf ./package/small-package/transmission-web-control
 # containerd Has验证
