e5bc87b4798be4e055f7037faf701efe8ec0c880
ChenChen <cc2286927@sina.cn>
2023-06-07 18:45:56 +0800

    Update diy-ARMv8.sh

--- diy-ARMv8.sh
+++ diy-ARMv8.sh
@@ -49,8 +49,8 @@ sed -i 's/services/system/g' feeds/luci/applications/luci-app-cpufreq/luasrc/con
 ## git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/openwrt-passwall2
 ## git clone --depth=1 -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 ## git clone --depth=1 https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dockerman feeds/luci/luci-app-dockerman
-svn export --force https://github.com/kiddin9/openwrt-packages/trunk/dockerd feeds/packages/utils/dockerd
+#svn export --force https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dockerman feeds/luci/luci-app-dockerman
+#svn export --force https://github.com/kiddin9/openwrt-packages/trunk/dockerd feeds/packages/utils/dockerd
 
 rm -rf feeds/luci/luci-app-aliyundrive-webdav
 rm -rf feeds/packages/aliyundrive-webdav
