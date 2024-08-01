1243d0cdfffcf2ec446026feb9ce6484e9159dd6
ChenChen <cc2286927@sina.cn>
2024-08-01 11:38:35 +0800

    Create rename.sh

---
+++ DIY/rename.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 指定目录
+dir="/opt/openwrt_packit/output"
+
+# 遍历目录中的文件
+for file in "$dir"/*; do
+    # 获取不带路径的文件名
+    filename=$(basename "$file")
+    
+    # 尝试替换指定的部分
+    new_filename="${filename//R24.?.?_k6.6.??-flippy-??+/k6.6-flippy+}"
+    
+    # 如果文件名发生了改变，则重命名文件
+    if [ "$filename" != "$new_filename" ]; then
+        mv "$file" "$dir/$new_filename"
+        echo "Renamed: $filename -> $new_filename"
+    fi
+done
