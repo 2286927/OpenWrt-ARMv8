c3687f1ae16e256454c01344146f3254b2f21b26
ChenChen <cc2286927@sina.cn>
2026-05-15 11:45:53 +0800

    package/pswcore/xray-core

--- DIY/diy-script.sh
+++ DIY/diy-script.sh
@@ -40,6 +40,7 @@ rm -rf feeds/luci/applications/luci-app-adguardhome
 rm -rf feeds/packages/net/adguardhome
 rm -rf package/luci-app-adguardhome
 rm -rf package/helloworld/xray-core
+rm -rf package/pswcore/xray-core
 # rm -rf feeds/packages/net/ddns-go
 
 # Git稀疏克隆，只克隆指定目录到本地
